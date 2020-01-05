94f7d2112da1c641558b024ae76ca324cb8c7ae0
marcomacca <m.macca91@gmail.com>
2020-01-05 15:45:46 +0100

    aggiunto file per classe veicolo

--- grafica.py
+++ grafica.py
@@ -2,7 +2,7 @@ import pygame
 
 # Init PyGame
 pygame.init()
-#costanti per schermata
+# costanti per schermata
 sfondo = pygame.image.load('imgGame\incrocio2png.png')
 sfondo = pygame.transform.scale(sfondo, (1024, 768))
 myfont = pygame.font.SysFont('Comic Sans MS', 30)
@@ -10,13 +10,15 @@ SCHERMO = pygame.display.set_mode((1024, 768))
 FPS = 60
 textsurface = myfont.render('', False, (0, 0, 0))  # testo per coordinate
 textsurface2 = myfont.render('', False, (0, 0, 0))  # testo per coordinate
+
+
 def aggiorna():
     pygame.display.update()
     pygame.time.Clock().tick(FPS)
     SCHERMO.fill((0, 0, 0))
 
 
-def disegna_oggetti(cars,testo1,testo2):
+def disegna_oggetti(cars, testo1, testo2):
     SCHERMO.blit(sfondo, (0, 0))
     SCHERMO.blit(testo1, (10, 100))
     SCHERMO.blit(testo2, (10, 10))
--- trafficSimulator.py
+++ trafficSimulator.py
@@ -1,243 +1,13 @@
 # Importo librerie
-import pygame
+import pygame,random
 from grafica import *
-import random
-
-
-
-# Carico Immagini
-car1img = pygame.image.load('imgGame\\auto.png')
-car2img = pygame.image.load('imgGame\\auto2.png')
-auto1 = pygame.transform.scale(car1img, (93, 46))
-auto2 = pygame.transform.scale(car2img, (93, 46))
+from veicolo import *
 
 
 # Costanti Globale
 
-Centro = (512, 384)
+
 listacar = list()
-LaneCoordinate = [[26, 500], [966, 220], [390, 16], [590, 730]]
-LaneCentrali = [[460, 16], [966, 316], [526, 730], [26, 420]]
-EndCoordinate = [[26, 220], [590, 16], [390, 730], [966, 500], [526, 16]]
-a = []
-
-
-class Car():
-    def __init__(self):
-        self.randomlane = random.choice([LaneCentrali, LaneCoordinate])
-        self.initcoordinate = random.choice(self.randomlane)
-        self.x = self.initcoordinate[0]
-        self.y = self.initcoordinate[1]
-        self.image = random.choice([auto1, auto2])
-        if self.x == 966:
-            self.image = pygame.transform.rotate(self.image, 180)
-        if self.x == 390 or self.x == 460:
-            self.image = pygame.transform.rotate(self.image, -90)
-        if self.x == 590 or self.x == 526:
-            self.image = pygame.transform.rotate(self.image, 90)
-        self.rect = self.image.get_rect()
-        self.angle = 0
-        self.speedx = 2
-        self.speedy = 2
-        self.saved_image = self.image.copy()
-        self.pos = (self.x, self.y)
-        self.startpos = None
-        self.lane = None
-        self.arrivo = None
-        self.ingombro = None
-
-
-    def randomCoordinateEND(self):
-        a = [x for x in EndCoordinate if x[0] !=
-             self.initcoordinate[0] and x[1] != self.initcoordinate[1]]
-        self.endcoordinate = random.choice(a)
-        self.arrivox, self.arrivoy = self.endcoordinate[0], self.endcoordinate[1]
-        # self.arrivoy = self.endcoordinate[1]
-
-    def checklane(self):
-        if self.startpos == 'destra' or self.startpos == 'sinistra':
-            if abs(self.initcoordinate[1]-Centro[1]) > 115:
-                self.lane = 'esterna'
-            else:
-                self.lane = 'centro'
-        if self.startpos == 'alto' or self.startpos == 'basso':
-            if abs(self.initcoordinate[0]-Centro[0]) > 70:
-                self.lane = 'esterna'
-            else:
-                self.lane = 'centro'
-
-    def changelane(self):  # interno esterno
-        if self.startpos == 'destra' or self.startpos == 'sinistra':
-            # si sta muovendo
-            if self.lane != 'esterna' and self.initcoordinate[0] != self.x:
-                if Centro[1] > self.initcoordinate[1]:  # alto a alto destra
-                    if abs(self.y - Centro[1]) < 164:
-                        self.y -= self.speedy
-                elif Centro[1] < self.initcoordinate[1]:  # basso a basso destra
-                    if abs(self.y - Centro[1]) < 116:
-                        self.y += self.speedy
-        if self.startpos == 'alto' or self.startpos == 'basso':
-            # si sta muovendo
-            if self.lane != 'esterna' and self.initcoordinate[1] != self.y:
-                if Centro[0] > self.initcoordinate[0]:  # alto a alto destra
-                    if abs(self.x - Centro[0]) < 122:
-                        self.x -= self.speedx
-                elif Centro[0] < self.initcoordinate[0]:  # basso a basso destra
-                    if abs(self.x - Centro[0]) < 78:
-                        self.x += self.speedx
-
-    def changelanerevers(self):  # esterno interno
-        if self.startpos == 'destra' or self.startpos == 'sinistra':
-            # si sta muovendo
-            if self.lane == 'esterna' and self.initcoordinate[0] != self.x:
-                if Centro[1] > self.initcoordinate[1]:  # destra
-                    if abs(self.y - Centro[1]) > 70:
-                        self.y += self.speedy
-                elif Centro[1] < self.initcoordinate[1]:  # sinistra
-                    if abs(self.y - Centro[1]) > 48:
-                        self.y -= self.speedy
-        if self.startpos == 'alto' or self.startpos == 'basso':
-            # si sta muovendo
-            if self.lane == 'esterna' and self.initcoordinate[1] != self.y:
-                if Centro[0] > self.initcoordinate[0]:  # alto
-                    if abs(self.x - Centro[0]) > 52:
-                        self.x += self.speedx
-                elif Centro[0] < self.initcoordinate[0]:  # basso
-                    if abs(self.x - Centro[0]) > 12:
-                        self.x -= self.speedx
-
-    def percorsoArrivo(self):
-
-        # alto
-        if self.arrivox > Centro[0] and self.arrivoy < Centro[1]:
-            if self.startpos == 'basso':
-                self.y -= self.speedy
-            self.arrivo = 'alto'
-        # destra
-        if self.arrivox > Centro[0] and self.arrivoy > Centro[1]:
-            if self.startpos == 'sinistra':
-                self.x += self.speedx
-            self.arrivo = 'destra'
-        # sinistra
-        if self.arrivox < Centro[0] and self.arrivoy < Centro[1]:
-            if self.startpos == 'destra':
-                self.x -= self.speedx
-            self.arrivo = 'sinistra'
-        # basso
-        if self.arrivox < Centro[0] and self.arrivoy > Centro[1]:
-            if self.startpos == 'alto':
-                self.y += self.speedy
-            self.arrivo = 'basso'
-
-    def definestarpos(self):
-        # ritorna sia corsia centrale o esterna più quadrante
-        # destra
-        if self.initcoordinate[0] > Centro[0] and self.initcoordinate[1] < Centro[1]:
-            self.startpos = 'destra'
-        # basso
-        if self.initcoordinate[0] > Centro[0] and self.initcoordinate[1] > Centro[1]:
-            self.startpos = 'basso'
-        # alto
-        if self.initcoordinate[0] < Centro[0] and self.initcoordinate[1] < Centro[1]:
-            self.startpos = 'alto'
-        # sinistra
-        if self.initcoordinate[0] < Centro[0] and self.initcoordinate[1] > Centro[1]:
-            self.startpos = 'sinistra'
-        self.checklane()
-    def rotatedx(self,angle):
-        self.image = pygame.transform.rotate(self.saved_image, self.angle)
-        self.rect = self.image.get_rect(center=self.rect.center)
-        self.angle -= angle
-    def rotatesx(self,angle):
-        self.image = pygame.transform.rotate(self.saved_image, self.angle)
-        self.rect = self.image.get_rect(center=self.rect.center)
-        self.angle += angle
-    def move(self):
-        self.pos = (self.x, self.y)
-        self.percorsoArrivo()
-        if self.startpos == 'alto' and self.arrivo == 'sinistra':
-            if self.y < self.arrivoy:
-                self.y += self.speedy
-            if self.y == self.arrivoy and self.angle != -95:
-                self.rotatedx(5)
-            if self.angle == -95:
-                if self.x > self.arrivox:
-                    self.x -= self.speedx
-            if self.lane == 'centro':
-                self.changelane()
-        if self.startpos == 'alto' and self.arrivo == 'destra':
-            if self.y < self.arrivoy:
-                self.y += self.speedy
-            if self.y == self.arrivoy and self.angle != 95:
-                self.rotatesx(5)
-            if self.angle == 95:
-                if self.x < self.arrivox:
-                    self.x += self.speedx
-            if self.lane == 'esterna':
-                self.changelanerevers()
-        if self.startpos == 'basso' and self.arrivo == 'sinistra':
-            if self.y > self.arrivoy:
-                self.y -= self.speedy
-            if self.y == self.arrivoy and self.angle != 95:
-                self.rotatesx(5)
-            if self.angle == 95:
-                if self.x > self.arrivox:
-                    self.x -= self.speedx
-            if self.lane == 'esterna':
-                self.changelanerevers()
-        if self.startpos == 'basso' and self.arrivo == 'destra':
-            if self.y > self.arrivoy:
-                self.y -= self.speedy
-            if self.y == self.arrivoy and self.angle != -95:
-                self.rotatedx(5)
-            if self.angle == -95:
-                if self.x < self.arrivox:
-                    self.x += self.speedx
-            if self.lane == 'centro':
-                self.changelane()
-
-        if self.startpos == 'sinistra' and self.arrivo == 'alto':
-            if self.x < self.arrivox:
-                self.x += self.speedx
-            if self.x == self.arrivox and self.angle != 95:
-                self.rotatesx(5)
-            if self.angle == 95:
-                if self.y > self.arrivoy:
-                    self.y -= self.speedy
-            if self.lane == 'esterna':
-                self.changelanerevers()
-        if self.startpos == 'sinistra' and self.arrivo == 'basso':
-            if self.x < self.arrivox:
-                self.x += self.speedx
-            if self.x == self.arrivox and self.angle != -95:
-                self.rotatedx(5)
-            if self.angle == -95:
-                if self.y < self.arrivoy:
-                    self.y += self.speedy
-            if self.lane == 'centro':
-                self.changelane()
-        if self.startpos == 'destra' and self.arrivo == 'alto':
-            if self.x > self.arrivox:
-                self.x -= self.speedx
-            if self.x == self.arrivox and self.angle != -95:
-                self.rotatedx(5)
-            if self.angle == -95:
-                if self.y > self.arrivoy:
-                    self.y -= self.speedy
-            if self.lane == 'centro':
-                self.changelane()
-        if self.startpos == 'destra' and self.arrivo == 'basso':
-            if self.x > self.arrivox:
-                self.x -= self.speedx
-            if self.x == self.arrivox and self.angle != 95:
-                self.rotatesx(5)
-            if self.angle == 95:
-                if self.y < self.arrivoy:
-                    self.y += self.speedy
-            if self.lane == 'esterna':
-                self.changelanerevers()
-
-
 
 
 def inizializza(Numerocars):
@@ -250,7 +20,7 @@ def inizializza(Numerocars):
         a.randomCoordinateEND()
         listacar.append(a)
 
- 
+
 inizializza(1)
 # initcordinate solo per test
 x = 0
@@ -270,14 +40,15 @@ def checkCollision(T):
                 # return 1
     return 0
 
+
 while True:
-     
-    disegna_oggetti(listacar,textsurface,textsurface2)
+
+    disegna_oggetti(listacar, textsurface, textsurface2)
     for car in listacar:
         car.move()
         car.ingombro = pygame.Rect(car.x, car.y, car.rect[2], car.rect[3])
         # rimuovo le auto che sono arrivate a destinazione
-        if car.arrivox == car.x and car.arrivoy == car.y:
+        if car.arrived:
             listacar.remove(car)
     textsurface2 = myfont.render('start'+str(listacar[0].ingombro), False, (0, 0, 0))
     textsurface = myfont.render('collisioni'+str(dsds), False, (0, 0, 0))
@@ -292,6 +63,7 @@ while True:
         if event.type == pygame.MOUSEBUTTONUP:
             inizializza(1)
             print("click")
-        if event.type == pygame.QUIT:  # gestisco la chiusura della finestra
+        # gestisco la chiusura della finestra    
+        if event.type == pygame.QUIT:  
             pygame.quit()
             SystemExit
